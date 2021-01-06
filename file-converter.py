f47cc34d7d863fab1571572fe630be65e1c6661f
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2021-01-06 11:42:21 +0530

    completed and tested

--- file-converter.py
+++ file-converter.py
@@ -1,15 +1,19 @@
 import os
 
-files = os.listdir()
-
 dest_source = input("Destination of source file: ")
 
 os.system(f"cd {dest_source}")
 
+files = os.listdir(dest_source)
+
 dest_conv = input("Destination of converted file: ")
 
-file_format = input((dest_conv+"> Format to you want the file converted: ")
+try:
+	os.system(f"mkdir {dest_conv}")
+except:
+	pass
 
-for file in range(len(files)):
-	os.system(f"ffmpeg -i {file} -vcodec copy -acodec copy {dest}/{file.split('.')[0]}.{file_format}") 
+file_format = input((dest_conv+"> Format to you want the file converted: "))
 
+for file in files:
+	os.system(f'ffmpeg -i \"{dest_source}/{file}\" -vcodec copy -acodec copy \"{dest_conv}/{file.split(".")[0]}.{file_format}\"')
