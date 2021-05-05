4452ae0ebc79fbd8714bc5171d92a31d7cf1b6c2
r(0_o)t <50882504+01010000-kumar@users.noreply.github.com>
2021-05-05 22:15:50 +0530

    added funtionailty for mp3 support and included ffmpeg files

--- file-converter.py
+++ file-converter.py
@@ -1,5 +1,7 @@
 import os
 
+ffmpeg = "ffmpeg\\bin\\"
+
 dest_source = input("Destination of source file: ")
 
 os.system(f"cd {dest_source}")
@@ -15,5 +17,10 @@ except:
 
 file_format = input((dest_conv+"> Format to you want the file converted: "))
 
-for file in files:
-	os.system(f'ffmpeg -i \"{dest_source}/{file}\" -vcodec copy -acodec copy \"{dest_conv}/{file.split(".")[0]}.{file_format}\"')
+if "mp3" in file_format:
+	for file in files:
+		os.system(f'ffmpeg -i \"{dest_source}/{file}\" \"{dest_conv}/{file.split(".")[0]}.{file_format}\"')
+else:
+	for file in files:
+		os.system(f'ffmpeg -i \"{dest_source}/{file}\" -vcodec copy -acodec copy \"{dest_conv}/{file.split(".")[0]}.{file_format}\"')
+
